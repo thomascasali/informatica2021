ac50e50f539d447f831c598955adc80408d91309
Thomas Casali <casali.thomas@gmail.com>
2021-10-09 12:41:56 +0200

    classe Rettangolo

---
+++ classeRettangolo/Classi.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classeRettangolo
+{
+    class Rettangolo
+    {
+        double lato1;
+        double lato2;
+
+        public Rettangolo()
+        {
+            lato1 = 0;
+            lato2 = 0;
+        }
+
+        public Rettangolo(double l1, double l2)
+        {
+            if (l1 > 0) {
+                lato1 = l1;
+            }
+            else
+            {
+                lato1 = 0;
+            }
+
+            if (l2 > 0)
+            {
+                lato2 = l2;
+            }
+            else
+            {
+                lato2 = 0;
+            }
+
+        }
+
+        public double Perimetro()
+        {
+            return lato1 * 2 + lato2 * 2;
+        }
+
+        public double Area()
+        {
+            return lato1 * lato2;
+        }
+
+        public void SetLato1(double l)
+        {
+            if (l > 0)
+            {
+                lato1 = l;
+            }
+        }
+        public void SetLato2(double l)
+        {
+            if (l > 0)
+            {
+                lato2 = l;
+            }
+        }
+        public double GetLato1()
+        {
+            return lato1;
+        }
+        public double GetLato2()
+        {
+            return lato2;
+        }
+    }
+}
---
+++ classeRettangolo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace classeRettangolo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //istanza dell'oggetto rettangolo
+            Rettangolo r = new Rettangolo(10, 5);
+            
+            //stampa Area e Perimetro
+            Console.WriteLine("Perimetro: "+r.Perimetro().ToString());
+            Console.WriteLine("Area: "+r.Area().ToString());
+
+            //provo ad impostare un lato negativo
+            r.SetLato2(-10);
+            //la stampa dimostra come la SetLato funziona correttamente e non permette l'immissione di valori negativi
+            Console.WriteLine("Perimetro: " + r.Perimetro().ToString());
+            Console.WriteLine("Area: " + r.Area().ToString());
+
+            //utilizzo i metodi per l'impostazione dei lati
+            r.SetLato1(20);
+            r.SetLato2(12);
+            //stampo i valori con i metodi per la lettura dei lati
+            Console.WriteLine("Lato1: "+r.GetLato1() + " Lato2:" + r.GetLato2());
+            Console.WriteLine("Perimetro: " + r.Perimetro().ToString());
+            Console.WriteLine("Area: " + r.Area().ToString());
+
+        }
+    }
+}
