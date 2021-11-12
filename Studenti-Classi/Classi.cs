5c5ec4595d57b107502ff4cbcc631a6dcc49c8d8
Thomas Casali <casali.thomas@gmail.com>
2021-11-12 08:49:53 +0100

    Studenti-Classi
    associazione di studenti alla classe

---
+++ Studenti-Classi/Classi.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Studenti_Classi
+{
+    class Classe
+    {
+        int anno;
+        string sezione;
+        string indirizzo;
+        List<Studente> studenti;
+
+        public Classe()
+        {
+            anno = DateTime.Now.Year;
+            studenti = new List<Studente>();
+        }
+
+        public Classe(int anno, string sezione, string indirizzo)
+        {
+            this.anno = anno;
+            this.sezione = sezione;
+            this.indirizzo = indirizzo;
+            studenti = new List<Studente>();
+        }
+
+        public string InfoClasse
+        {
+            get { return anno.ToString() + sezione; }
+        }
+        public void AddStudente(Studente s)
+        {
+            studenti.Add(s);
+        }
+
+        public string StampaStudenti()
+        {
+            string s = "";
+            foreach(Studente stud in studenti)
+            {
+                s = s + stud.NomeCognome + "\n";
+            }
+            return s;
+        }
+    }
+
+    class Studente
+    {
+        string nome;
+        string cognome;
+        string cf;
+        DateTime datanascita;
+        string email;
+
+        public Studente(string nome, string cognome)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+        }
+
+        public string NomeCognome
+        {
+            get { return nome + " " + cognome ; }
+        }
+    }
+}
---
+++ Studenti-Classi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Studenti_Classi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Classe c = new Classe(4, "H", "Informatica");
+            Studente s1 = new Studente("Primo", "Studente");
+            Studente s2= new Studente("Secondo","Studente");
+            Studente s3= new Studente("Terzo","Studente");
+            c.AddStudente(s1);
+            c.AddStudente(s2);
+            c.AddStudente(s3);
+
+            // altro metodo per la creazione dello studente e aggiunta nella classe
+            c.AddStudente(new Studente("Quarto", "Studente"));
+
+            Console.WriteLine("Elenco degli studenti della classe " + c.InfoClasse +"\n" + c.StampaStudenti());
+        }
+    }
+}
