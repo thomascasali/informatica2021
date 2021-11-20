4dcabe1c6b7edaaa8fb9a26a2bd9aef4281c28a6
Thomas Casali <casali.thomas@gmail.com>
2021-11-20 10:54:06 +0100

    esempio di associazione con classe automobile

---
+++ Automobile/Classi.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Automobile
+{
+    class Automobile
+    {
+        string targa;
+        DateTime dataImmatricolazione;
+        public Motore motore;
+        public Telaio telaio;
+        public Ruota[] ruote;
+
+        public Automobile()
+        {
+            ruote = new Ruota[4]; //specifico che l'automobile ha 4 ruote
+        }
+
+        public Automobile(string targa, Motore m, Telaio t, Ruota r1, Ruota r2, Ruota r3, Ruota r4)
+        {
+            this.targa = targa;
+            dataImmatricolazione = DateTime.Now;
+            motore = m;
+            telaio = t;
+            ruote = new Ruota[4];
+            ruote[0] = r1;
+            ruote[1] = r2;
+            ruote[2] = r3;
+            ruote[3] = r4;
+        }
+    }
+
+    class Motore
+    {
+        int cilindrata;
+        string alimentazione;
+
+        public Motore(int cil, string alim)
+        {
+            cilindrata = cil;
+            alimentazione = alim;
+        }
+
+        public int Cilindrata
+        {
+            get { return cilindrata; }
+        }
+
+        public string Alimentazione
+        {
+            get { return alimentazione; }
+        }
+    }
+
+    class Telaio
+    {
+        string numTealio;
+        string materiale;
+        
+        public Telaio()
+        {
+            numTealio = "00000";
+            materiale = "acciaio";
+        }
+
+        public Telaio(string numTelaio, string materiale)
+        {
+            this.numTealio = numTelaio;
+            this.materiale = materiale;
+        }
+
+
+        public string NumTelaio
+        {
+            get { return numTealio; }
+        }
+
+    }
+
+    class Ruota
+    {
+        string tipo; //tradizionale, 4 stagioni, invernale
+        int diametro;
+        float pressione;
+
+        public Ruota()
+        {
+            tipo = "tradizionale";
+            diametro = 40;
+        }
+        public Ruota(string tipoRuota, int diametroRuota)
+        {
+            tipo = tipoRuota;
+            diametro = diametroRuota;
+            pressione = 2.9;
+        }
+
+        public string Tipo
+        {
+            get { return tipo; }
+        }
+
+        public int Pressione
+        {
+            get { return pressione; }
+        }
+
+        public int Diametro
+        {
+            get
+            {
+                return diametro;
+            }
+
+            set
+            {
+                if (value > 0 && value < 100) diametro = value;
+            }
+        }
+
+
+    }
+
+}
---
+++ Automobile/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Automobile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ruota ruota1 = new Ruota("invernale", 45);
+            Ruota ruota2 = new Ruota("invernale", 45);
+            Ruota ruota3 = new Ruota("invernale", 45);
+            Automobile a = new Automobile("DZ122RS",new Motore(1600, "benzina"),new Telaio(),ruota1,ruota2,ruota3, new Ruota("invernale", 45));
+            Console.WriteLine(a.motore.Cilindrata);
+        }
+    }
+}
