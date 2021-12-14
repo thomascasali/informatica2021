7e9b3c7790966d9d605394f5fbc2f93c691653b4
Thomas Casali <casali.thomas@gmail.com>
2021-12-14 08:24:03 +0100

    gestione pacchi

---
+++ gestionePacchi/ClassiPacco.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestionePacchi
+{
+    class Pacco
+    {
+        public Mittente mittente;
+        public Destinatario destinatario;
+        protected double altezza;
+        protected double lunghezza;
+        protected double larghezza;
+        protected double peso;
+        protected string tipo;
+
+        public Pacco(Mittente mittente, Destinatario destinatario, double altezza, double lunghezza,double larghezza,double peso)
+        {
+            this.mittente=mittente;
+            this.destinatario=destinatario;
+            this.altezza=altezza;
+            this.lunghezza=lunghezza;
+            this.larghezza=larghezza;
+            this.peso=peso;
+        }
+
+        public string Tipo
+        {
+            get { return tipo; }
+        }
+        protected double Volume()
+        {
+            return larghezza * altezza * lunghezza;
+        }
+
+        public virtual double Costo()
+        {
+            return 0;
+        }
+    }
+    class PaccoCelere1 : Pacco
+    {
+        public PaccoCelere1(Mittente mittente, Destinatario destinatario, double altezza, double lunghezza, double larghezza, double peso) : 
+            base(mittente, destinatario, altezza, lunghezza, larghezza, peso)
+        {
+            tipo = "Pacco Celere 1";
+        }
+
+        public override double Costo()
+        {
+            double costo;
+            costo = Volume() * peso * 2;
+            return costo;
+        }
+    }
+
+    class PaccoCelere3 : Pacco
+    {
+        public PaccoCelere3(Mittente mittente, Destinatario destinatario, double altezza, double lunghezza, double larghezza, double peso) : base(mittente, destinatario, altezza, lunghezza, larghezza, peso)
+        {
+            tipo = "Pacco Celere 3";
+        }
+
+        public override double Costo()
+        {
+            double costo;
+            costo = Volume() * peso * 1.5;
+            return costo;
+        }
+    }
+
+    class PaccoAssicurato : Pacco
+    {
+        double valoreAssicurato;
+        public PaccoAssicurato(double valoreAssicurato, Mittente mittente, Destinatario destinatario, double altezza, double lunghezza, double larghezza, double peso) : base(mittente, destinatario, altezza, lunghezza, larghezza, peso)
+        {
+            tipo = "Pacco Assicurato";
+            this.valoreAssicurato = valoreAssicurato;
+        }
+
+        public override double Costo()
+        {
+            double costo;
+            costo = Volume() * peso * 1.5 + 10/100*valoreAssicurato;
+            return costo;
+        }
+    }
+
+    class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Indirizzo { get; set; }
+        public string Cap { get; set; }
+        public string Citta { get; set; }
+        public string Prov { get; set; }
+
+        public Persona(string Nome, string Cognome, string Indirizzo, string Cap, string Citta, string Prov)
+        {
+            this.Nome = Nome;
+            this.Cognome = Cognome;
+            this.Indirizzo = Indirizzo;
+            this.Cap = Cap;
+            this.Citta = Citta;
+            this.Prov = Prov;
+        }
+
+        public string GetInfo()
+        {
+            return Cognome + " " + Nome + " " + Indirizzo + " (" + Cap + ") " + Citta + " (" + Prov + ") ";
+        }
+    }
+    
+    class Mittente : Persona
+    {
+        public Mittente(string Nome, string Cognome, string Indirizzo, string Cap, string Citta, string Prov) :
+            base(Nome, Cognome, Indirizzo, Cap, Citta, Prov)
+        {
+           
+        }
+    }
+
+    class Destinatario : Persona
+    {
+        public Destinatario(string Nome, string Cognome, string Indirizzo, string Cap, string Citta, string Prov) :
+            base(Nome, Cognome, Indirizzo, Cap, Citta, Prov)
+        {
+
+        }
+
+    }
+}
---
+++ gestionePacchi/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestionePacchi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pacco> pacchi = new List<Pacco>();
+            //...... completare......
+
+
+            Mittente mit = new Mittente("thomas","casali","via roma","47023","Cesena","FC");
+            Destinatario des = new Destinatario("mario", "rossi", "via bologna", "47900", "Rimini", "RN");
+            PaccoCelere1 pc1 = new PaccoCelere1(mit, des, 10, 10, 10, 1);
+            Console.WriteLine(pc1.Tipo + ":");
+            Console.WriteLine("Mittente: " + pc1.mittente.GetInfo());
+            Console.WriteLine("Destinatario: "+ pc1.destinatario.GetInfo());
+            Console.WriteLine("Costo spedizione: " + pc1.Costo());
+            Console.WriteLine();
+
+            PaccoCelere3 pc3 = new PaccoCelere3(mit, des, 10, 10, 10, 1);
+            Console.WriteLine(pc3.Tipo + ":");
+            Console.WriteLine("Mittente: " + pc3.mittente.GetInfo());
+            Console.WriteLine("Destinatario: " + pc3.destinatario.GetInfo());
+            Console.WriteLine("Costo spedizione: " + pc3.Costo());
+
+            Console.ReadKey();
+        }
+    }
+}
