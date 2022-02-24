e606b5e5df59a072f6faf8d9cc7d07b7a2a27e4c
Thomas Casali <casali.thomas@gmail.com>
2022-02-24 08:17:50 +0100

    aggiornamento classi centro di ricerca

---
+++ CentroRicercaClassi/ClassiCR.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CentroRicerca
+{
+    class CentroDiRicerca
+    {
+        public string Nome { get; set; }
+        public List<AreaDiRicerca> aree;
+    }
+
+    class AreaDiRicerca
+    {
+        public string NomeArea { get; set; }
+        public ResposabileArea Responsabile { get; set; }
+        public List<Ricercatore> Ricercatori;
+        public List<Progetto> ProgettiArea;
+    }
+    class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+
+    }
+
+    class ResposabileArea: Persona
+    {
+        public string Qualifica { get; set; }
+    }
+
+    class Ricercatore : Persona
+    {
+        public string LaureatoIn { get; set; }
+        public Team TeamDiRicerca { get; }
+    }
+
+    class Junior : Ricercatore
+    {
+    }
+    class Senior : Ricercatore
+    {
+    }
+
+    class Progetto
+    {
+        public string NomeProgetto { get; set; }
+        public Team TeamProgetto { get; set; }
+    }
+
+    class Team
+    {
+        public string NomeTeam { get; set; }
+        public Senior ResponsabileTeam { get; set; }
+        public List<Junior> RicercatoriTeam;
+    }
+
+}
